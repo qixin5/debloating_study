e9fd2399c5340b4db50c517a676dbc0639e65586
yjh <yuanjiahao@whu.edu.cn>
2025-06-04 14:38:00 +0800

    Fix testscript.

--- expt/debaug/benchmark/gzip-1.3/testscript/util/cleanup.sh
+++ expt/debaug/benchmark/gzip-1.3/testscript/util/cleanup.sh
@@ -8,7 +8,7 @@ if [ -z $INDIR ]; then
 fi
 
 rm -rf $INDIR
-cp -r ~/debaug_expt/debaug/benchmark/gzip-1.3/input.origin/all $INDIR || { echo "$0: Cannot replenish inputs directory"; exit 1; }
+cp -r $DEBSTUDY_DIR/expt/debaug/benchmark/gzip-1.3/input.origin/all $INDIR || { echo "$0: Cannot replenish inputs directory"; exit 1; }
 chmod -R u+w $INDIR
 
 chmod 000 $INDIR/testdir/nopermissionfile || { echo "$0: Cannot change permission for nopermissionfile"; exit 1; }
